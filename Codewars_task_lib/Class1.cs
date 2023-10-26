0f1a0a873c58e058e8019047590fa2d2856f2688
Zaurbek Malsagov <work_menos@outlook.com>
2023-10-26 11:02:05 +0300

    Add project files.

---
+++ Codewars_task_lib/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_task_lib
+{
+    public  class CodewarsLib
+    {
+
+        /*
+         Create a function which answers the question "Are you playing banjo?".
+            If your name starts with the letter "R" or lower case "r", you are playing banjo!
+            The function takes a name as its only argument, and returns one of the following strings
+         */
+        public static string AreYouPlayingBanjo(string name) {
+            string result = string.Empty;
+            char[] name_array = name.ToCharArray();
+            //Implement me
+            if ((char.ToLower(name_array[0])) == 'r') {
+                result = name + " plays banjo";
+            }
+            else {
+                result = name + " does not play banjo";
+            }
+            return result;
+        }
+    }
+}
---
+++ Codewars_tasks_training_c_sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codewars_task_lib;
+
+namespace Codewars_tasks_training_c_sharp {
+    internal class Program {
+        static void Main(string[] args) {
+            string name = "Martin";
+            name = CodewarsLib.AreYouPlayingBanjo(name);
+            
+        }
+
+        
+    }
+}
