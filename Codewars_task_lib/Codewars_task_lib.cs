ff8755be32b007042c9e399f05a30ca68b5007c1
Zaurbek Malsagov <lordmalsagov@gmail.com>
2024-01-30 15:21:37 +0300

    add narcissic number

--- Codewars_task_lib/Codewars_task_lib.cs
+++ Codewars_task_lib/Codewars_task_lib.cs
@@ -128,6 +128,44 @@ namespace Codewars_task_lib {
         }
 
 
+
+
+
+        /*A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. 
+         * In this Kata, we will restrict ourselves to decimal (base 10).
+         * For example, take 153 (3 digits), which is narcissistic:
+         * 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+         * and 1652 (4 digits), which isn't:
+         *  1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+         * Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+         * https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/csharp
+         */
+
+
+        public static bool Narcissistic(int value) {
+            // Code me
+            int original_number = value;
+            bool result = false;
+            double result_number = 0;
+            List<int> values = new List<int>();
+            while (value > 0) {
+                if (value != 0) {
+                    values.Add(value % 10);
+                    value = value / 10;
+
+                }
+            }
+
+            foreach (var item in values) {
+                result_number += Math.Pow(item, values.Count);
+            }
+            if ((double)original_number == result_number) {
+                result = true;
+            }
+            return result;
+
+        }
+
     }
 }
 
